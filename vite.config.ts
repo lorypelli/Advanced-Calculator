af4fb1a350dadac0c1233d85e1211b77f6457f37
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-02-14 22:28:16 +0100

    started advanced calculator

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+export default defineConfig({
+    plugins: [ViteMinifyPlugin()],
+    server: {
+        open: true,
+        host: '127.0.0.1'
+    },
+    build: {
+        minify: 'terser',
+        terserOptions: {
+            compress: {
+                ecma: 2020,
+                passes: 10
+            }
+        },
+        rollupOptions: {
+            output: {
+                dir: './dist',
+                entryFileNames: 'index.js',
+                assetFileNames: 'style.css',
+                compact: true
+            }
+        }
+    }
+});
