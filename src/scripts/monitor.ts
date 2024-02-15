9b1f81c94ff155884d056265b57b4e197b4f426a
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-02-15 20:00:31 +0100

    continue working on calculator

--- src/scripts/monitor.ts
+++ src/scripts/monitor.ts
@@ -2,8 +2,23 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach(b => b.addEventListener('click', () => {
     const result = document.querySelector('#result');
     if (result) {
+        const arr = result.innerHTML.split('');
         if (b.classList.contains('primary') || b.classList.contains('secondary')) {
-            result.innerHTML += b.innerHTML;
+            if (arr[arr.length - 2] == '+' || arr[arr.length - 2] == '-' || arr[arr.length - 2] == '×' || arr[arr.length - 2] == '÷') {
+                if (arr[arr.length - 3] == '+' || arr[arr.length - 3] == '-' || arr[arr.length - 3] == '×' || arr[arr.length - 3] == '÷') {
+                    arr[arr.length - 2] = arr[arr.length - 3];
+                    arr[arr.length - 3] = '';
+                }
+            }
+            for (let i = 0; i < arr.length; i++) {
+                if (arr[i] == '|') {
+                    const temp = '|';
+                    arr[i] = b.innerHTML;
+                    arr[i + 1] = temp;
+                    break;
+                }
+            }
+            result.innerHTML = arr.join('');
         }
     }
 }));
