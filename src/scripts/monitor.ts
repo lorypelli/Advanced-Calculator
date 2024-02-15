d98fbe529c3508bfeb4fd553a83ded601925cc51
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-02-15 21:37:04 +0100

    clear and del functions

--- src/scripts/monitor.ts
+++ src/scripts/monitor.ts
@@ -23,8 +23,24 @@ buttons.forEach(b => b.addEventListener('click', () => {
                     arr.splice(3, 1);
                 }
             }
-            result.innerHTML = arr.join('');
         }
+        switch (b.id) {
+        case 'del': {
+            for (let i = 0; i < arr.length; i++) {
+                if (arr[0] != '|' && arr[i] == '|') {
+                    arr.splice(i - 1, 1);
+                    break;
+                }
+            }
+            break;
+        }
+        case 'clear': {
+            arr.length = 1;
+            arr[0] = '|';
+            break;
+        }
+        }
+        result.innerHTML = arr.join('');
     }
 }));
 export {};
